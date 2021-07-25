af573711be0d93a321e6e3b6ef6331c96f0ce785
Dharma <dharmaguadeloupe@gmail.com>
2021-07-25 16:39:08 +0100

    Exercises A-list-of-names

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,6 +1,16 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
-}
+
+  arrayOfPeople.forEach(person => {
+    const personName = document.createElement("h1");
+    personName.innerText = person.name;
+    content.appendChild(personName);
+
+    const jobTitle = document.createElement("h2");
+    jobTitle.innerText = person.job;
+    content.appendChild(jobTitle);
+  })
+};
 
 let people = [
   { name: "Chris", job: "Teacher" },
