78174f91a98df37cbc5f1879577f0c97c576d568
Dharma <dharmaguadeloupe@gmail.com>
2021-07-25 18:34:18 +0100

    Updated B-shopping-list with comments

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,11 +1,18 @@
 function shoppingList(arrayOfPeople) {
+  //create variable to access content <div>
  const content = document.getElementById("content");
+ //create unordered list element
  const unorderedList = document.createElement("ul");
+ //add <ul> to content <div> 
  content.appendChild(unorderedList);
 
+ //for loop to go through the array
  arrayOfPeople.forEach(item => {
+   //create <li> element
   const listItem = document.createElement("li");
+  //retrieve value of item each time and render on the webpage
   listItem.innerText = item;
+  //add <li> items to <ul>
   unorderedList.appendChild(listItem);
  })
  
